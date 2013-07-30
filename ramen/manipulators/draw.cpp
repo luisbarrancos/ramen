4baf410c507cf9c1b7427da8b97ce453237dcfb9
est77 <ramenhdr@gmail.com>
2013-07-30 21:55:10 +0200

    small GL cleanup

--- ramen/manipulators/draw.cpp
+++ ramen/manipulators/draw.cpp
@@ -8,9 +8,9 @@
 
 #include<OpenEXR/ImathMath.h>
 
-#include<ramen/GL/Imath_gl.hpp>
-
 #include<ramen/math/constants.hpp>
+
+#include<ramen/GL/gl.hpp>
 
 namespace ramen
 {
@@ -21,63 +21,63 @@ namespace
 
 void draw_arrow( const Imath::V2f& p, const Imath::V2f& q, float offset = 0)
 {
-	float l = ( q - p).length() / 7.0f;
-		
-	gl_begin( GL_LINES);
-		gl_vertex2f( p.x + offset, p.y + offset);
-		gl_vertex2f( q.x + offset, q.y + offset);
-	
-		Imath::V2f d( p - q);
-		d.normalize();
-		d *= l;
-	
-		float cs = Imath::Math<float>::cos( 30.0f * math::constants<double>::deg2rad());
-		float ss = Imath::Math<float>::sin( 30.0f * math::constants<double>::deg2rad());
-	
-		gl_vertex2f( q.x + offset, q.y + offset);
-		gl_vertex2f( q.x + ( d.x * cs - d.y * ss) + offset, q.y + ( d.x * ss + d.y * cs) + offset);
-	
-		cs = Imath::Math<float>::cos( -30.0 * math::constants<double>::deg2rad());
-		ss = Imath::Math<float>::sin( -30.0 * math::constants<double>::deg2rad());
-	
-		gl_vertex2f( q.x + offset, q.y + offset);
-		gl_vertex2f( q.x + ( d.x * cs - d.y * ss) + offset, q.y + ( d.x * ss + d.y * cs) + offset);
-	gl_end();
+    float l = ( q - p).length() / 7.0f;
+
+    gl_begin( GL_LINES);
+        gl_vertex2f( p.x + offset, p.y + offset);
+        gl_vertex2f( q.x + offset, q.y + offset);
+
+        Imath::V2f d( p - q);
+        d.normalize();
+        d *= l;
+
+        float cs = Imath::Math<float>::cos( 30.0f * math::constants<double>::deg2rad());
+        float ss = Imath::Math<float>::sin( 30.0f * math::constants<double>::deg2rad());
+
+        gl_vertex2f( q.x + offset, q.y + offset);
+        gl_vertex2f( q.x + ( d.x * cs - d.y * ss) + offset, q.y + ( d.x * ss + d.y * cs) + offset);
+
+        cs = Imath::Math<float>::cos( -30.0 * math::constants<double>::deg2rad());
+        ss = Imath::Math<float>::sin( -30.0 * math::constants<double>::deg2rad());
+
+        gl_vertex2f( q.x + offset, q.y + offset);
+        gl_vertex2f( q.x + ( d.x * cs - d.y * ss) + offset, q.y + ( d.x * ss + d.y * cs) + offset);
+    gl_end();
 }
 
 template<class V>
 void box_vertices( const Imath::Box<V>& box, float offset = 0)
 {
-	gl_vertex2f( box.min.x + offset, box.min.y + offset);
-	gl_vertex2f( box.max.x + offset, box.min.y + offset);
-	gl_vertex2f( box.max.x + offset, box.max.y + offset);
-	gl_vertex2f( box.min.x + offset, box.max.y + offset);
+    gl_vertex2f( box.min.x + offset, box.min.y + offset);
+    gl_vertex2f( box.max.x + offset, box.min.y + offset);
+    gl_vertex2f( box.max.x + offset, box.max.y + offset);
+    gl_vertex2f( box.min.x + offset, box.max.y + offset);
 }
 
 } // unnamed
 
 void draw_small_box( const Imath::V2f& p, float size)
 {
-	gl_begin( GL_LINE_LOOP);
-		gl_vertex2f( p.x - size, p.y - size);
-		gl_vertex2f( p.x + size, p.y - size);
-		gl_vertex2f( p.x + size, p.y + size);
-		gl_vertex2f( p.x - size, p.y + size);
-	gl_end();
+    gl_begin( GL_LINE_LOOP);
+        gl_vertex2f( p.x - size, p.y - size);
+        gl_vertex2f( p.x + size, p.y - size);
+        gl_vertex2f( p.x + size, p.y + size);
+        gl_vertex2f( p.x - size, p.y + size);
+    gl_end();
 }
 
 void draw_cross( const Imath::V2f& p, float xsize, float ysize, const Imath::Color3c& color, float pixel_scale)
 {
-	float off = shadow_offset( pixel_scale);
-	
+    float off = shadow_offset( pixel_scale);
+
     gl_begin( GL_LINES);
-		gl_color3ub( 0, 0, 0);
+        gl_color3ub( 0, 0, 0);
         gl_vertex2f( p.x - xsize + off, p.y + off);
         gl_vertex2f( p.x + xsize + off, p.y + off);
         gl_vertex2f( p.x + off, p.y - ysize + off);
         gl_vertex2f( p.x + off, p.y + ysize + off);
-	
-		gl_color( color);
+
+        gl_color3f( color.x, color.y, color.z);
         gl_vertex2f( p.x - xsize, p.y);
         gl_vertex2f( p.x + xsize, p.y);
         gl_vertex2f( p.x, p.y - ysize);
@@ -85,25 +85,25 @@ void draw_cross( const Imath::V2f& p, float xsize, float ysize, const Imath::Col
     gl_end();
 }
 
-void draw_ellipse( const Imath::V2f& center, float xradius, float yradius, const Imath::Color3c& color, 
-				   float pixel_scale, int steps)
+void draw_ellipse( const Imath::V2f& center, float xradius, float yradius, const Imath::Color3c& color,
+                   float pixel_scale, int steps)
 {
     float ang_inc = 360.0f / steps * math::constants<float>::deg2rad();
-	float off = shadow_offset( pixel_scale);
+    float off = shadow_offset( pixel_scale);
 
-	gl_color3ub( 0, 0, 0);
+    gl_color3ub( 0, 0, 0);
     gl_begin( GL_LINE_LOOP);
     for( float i = 0; i < steps; ++i)
-		gl_vertex2f( center.x + off + Imath::Math<float>::cos( i * ang_inc) * xradius, 
-					 center.y + off + Imath::Math<float>::sin( i * ang_inc) * yradius);
-	gl_end();
+        gl_vertex2f( center.x + off + Imath::Math<float>::cos( i * ang_inc) * xradius,
+                     center.y + off + Imath::Math<float>::sin( i * ang_inc) * yradius);
+    gl_end();
 
-	gl_color( color);
+    gl_color3f( color.x, color.y, color.z);
     gl_begin( GL_LINE_LOOP);
     for( float i = 0; i < steps; ++i)
-		gl_vertex2f( center.x + Imath::Math<float>::cos( i * ang_inc) * xradius, 
-					 center.y + Imath::Math<float>::sin( i * ang_inc) * yradius);
-	gl_end();
+        gl_vertex2f( center.x + Imath::Math<float>::cos( i * ang_inc) * xradius,
+                     center.y + Imath::Math<float>::sin( i * ang_inc) * yradius);
+    gl_end();
 }
 
 void gl_transformed_box( const Imath::Box2i& box, const Imath::M33d& m, float offset)
@@ -121,12 +121,12 @@ void gl_transformed_box( const Imath::Box2i& box, const Imath::M33d& m, float of
     gl_vertex2f( p.x + offset, p.y + offset);
 }
 
-void draw_bezier_curve( const Imath::V2f& a, const Imath::V2f& b, const Imath::V2f& c, const Imath::V2f& d, 
-						const Imath::Color3c& color, float pixel_scale, int steps)
+void draw_bezier_curve( const Imath::V2f& a, const Imath::V2f& b, const Imath::V2f& c, const Imath::V2f& d,
+                        const Imath::Color3c& color, float pixel_scale, int steps)
 {
-	float off = shadow_offset( pixel_scale);
-	
-	GLfloat ctrlpoints[4][3];
+    float off = shadow_offset( pixel_scale);
+
+    GLfloat ctrlpoints[4][3];
     ctrlpoints[0][0] = a.x + off;
     ctrlpoints[0][1] = a.y + off;
     ctrlpoints[0][2] = 0;
@@ -143,14 +143,14 @@ void draw_bezier_curve( const Imath::V2f& a, const Imath::V2f& b, const Imath::V
     ctrlpoints[3][1] = d.y + off;
     ctrlpoints[3][2] = 0;
 
-	// for some reason, this code generates 
-	// an OpenGL error, and Ramen crashes...
-	gl_color3ub( 0, 0, 0);
+    // for some reason, this code generates
+    // an OpenGL error, and Ramen crashes...
+    gl_color3ub( 0, 0, 0);
     glMap1f( GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
 
     glBegin( GL_LINE_STRIP);
     for (int i = 0; i <= steps; ++i)
-		glEvalCoord1f( ( GLfloat) i/ (float) steps);
+        glEvalCoord1f( ( GLfloat) i/ (float) steps);
     glEnd();
 
     ctrlpoints[0][0] = a.x;
@@ -169,42 +169,42 @@ void draw_bezier_curve( const Imath::V2f& a, const Imath::V2f& b, const Imath::V
     ctrlpoints[3][1] = d.y;
     ctrlpoints[3][2] = 0;
 
-	gl_color( color);
+    gl_color3f( color.x, color.y, color.z);
     glMap1f( GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
 
     glBegin( GL_LINE_STRIP);
     for (int i = 0; i <= steps; ++i)
-		glEvalCoord1f( ( GLfloat) i / (float) steps);
+        glEvalCoord1f( ( GLfloat) i / (float) steps);
     glEnd();
 }
 
 // axes
 void draw_xy_axes( const Imath::V2f& c, const Imath::V2f& x, const Imath::V2f& y, float xscale,
-				   const Imath::Color3c& xcol, const Imath::Color3c& ycol, float pixel_scale)
+                   const Imath::Color3c& xcol, const Imath::Color3c& ycol, float pixel_scale)
 {
-	
-	Imath::V2f xx( x.x * xscale + c.x, x.y + c.y);
-	Imath::V2f yy( y.x * xscale + c.x, y.y + c.y);
-	float offset = shadow_offset( pixel_scale);
 
-	gl_color3ub( 0, 0, 0);
+    Imath::V2f xx( x.x * xscale + c.x, x.y + c.y);
+    Imath::V2f yy( y.x * xscale + c.x, y.y + c.y);
+    float offset = shadow_offset( pixel_scale);
+
+    gl_color3ub( 0, 0, 0);
     draw_arrow( c, xx, offset);
     draw_arrow( c, yy, offset);
-	
-    gl_color( xcol);
+
+    gl_color3f( xcol.x, xcol.y, xcol.z);
     draw_arrow( c, xx);
 
-    gl_color( ycol);
+    gl_color3f( ycol.x, ycol.y, ycol.z);
     draw_arrow( c, yy);
 }
 
 void draw_xy_axes( const Imath::V2f& c, float xsize, float ysize, float angle, float xscale,
-				   const Imath::Color3c& xcol, const Imath::Color3c& ycol, float pixel_scale)
+                   const Imath::Color3c& xcol, const Imath::Color3c& ycol, float pixel_scale)
 {
     float cs = Imath::Math<float>::cos( (double) angle * math::constants<double>::deg2rad());
     float ss = Imath::Math<float>::sin( (double) angle * math::constants<double>::deg2rad());
-	draw_xy_axes( c, Imath::V2f( cs, ss) * xsize, Imath::V2f( ss, -cs) * ysize, xscale,
-					xcol, ycol, pixel_scale);
+    draw_xy_axes( c, Imath::V2f( cs, ss) * xsize, Imath::V2f( ss, -cs) * ysize, xscale,
+                    xcol, ycol, pixel_scale);
 }
 
 // util
@@ -212,39 +212,39 @@ float shadow_offset( float pixel_scale) { return 2.0f / pixel_scale;}
 
 void draw_box( const Imath::Box2i& box, const Imath::Color3c& color, float pixel_scale, bool draw_corners)
 {
-	Imath::Box2f box2( Imath::V2f( box.min), Imath::V2f( box.max));
-	draw_box( box2, color, pixel_scale, draw_corners);
+    Imath::Box2f box2( Imath::V2f( box.min), Imath::V2f( box.max));
+    draw_box( box2, color, pixel_scale, draw_corners);
 }
 
 void draw_box( const Imath::Box2f& box, const Imath::Color3c& color, float pixel_scale, bool draw_corners)
 {
-	float off = shadow_offset( pixel_scale);
+    float off = shadow_offset( pixel_scale);
 
-	// shadow
-	gl_color3ub( 0, 0, 0);
-	gl_begin( GL_LINE_LOOP);
-		box_vertices( box, off);
-	gl_end();
+    // shadow
+    gl_color3ub( 0, 0, 0);
+    gl_begin( GL_LINE_LOOP);
+        box_vertices( box, off);
+    gl_end();
 
-	if( draw_corners)
-	{
-		gl_begin( GL_POINTS);
-			box_vertices( box, off);
-		gl_end();
-	}
-	
-	// main
-    gl_color( color);
-	gl_begin( GL_LINE_LOOP);
-		box_vertices( box);
-	gl_end();
-	
-	if( draw_corners)
-	{
-		gl_begin( GL_POINTS);
-			box_vertices( box);
-		gl_end();
-	}	
+    if( draw_corners)
+    {
+        gl_begin( GL_POINTS);
+            box_vertices( box, off);
+        gl_end();
+    }
+
+    // main
+    gl_color3f( color.x, color.y, color.z);
+    gl_begin( GL_LINE_LOOP);
+        box_vertices( box);
+    gl_end();
+
+    if( draw_corners)
+    {
+        gl_begin( GL_POINTS);
+            box_vertices( box);
+        gl_end();
+    }
 }
 
 } // namespace
---
+++ ramen/ui/image2.cpp
@@ -0,0 +1,155 @@
+// Copyright (c) 2013 Esteban Tovagliari
+// Licensed under the terms of the CDDL License.
+// See CDDL_LICENSE.txt for a copy of the license.
+
+#include<ramen/ui/image2.hpp>
+
+#include<iostream>
+
+#include<boost/ptr_container/ptr_vector.hpp>
+#include<boost/bind.hpp>
+#include<boost/range/algorithm/for_each.hpp>
+
+#include<ramen/assert.hpp>
+
+namespace ramen
+{
+namespace ui
+{
+
+struct image2_t::impl
+{
+    struct tile_t
+    {
+        enum { tile_size = 256};
+
+        tile_t()
+        {
+            texture_id_ = gl_gen_texture();
+            gl_bind_texture( GL_TEXTURE_2D, texture_id_);
+            gl_tex_parameteri( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            gl_tex_parameteri( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            gl_tex_parameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+            gl_tex_parameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+            gl_tex_image2d( GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, 0);
+        }
+
+        void draw() const
+        {
+            gl_bind_texture( GL_TEXTURE_2D, texture_id_);
+            // draw quad here...
+        }
+
+        GLuint texture_id_;
+        math::point2i_t pos;
+        math::vector2i_t size;
+    };
+
+    impl() {}
+
+    void reset()
+    {
+        display_window_ = math::box2i_t();
+        data_window_ = math::box2i_t();
+        pixels_.clear();
+        tiles_.clear();
+    }
+
+    void reset( image::buffer_t pixels,
+                const math::box2i_t& display_window,
+                const math::box2i_t& data_window)
+    {
+        pixels_ = pixels;
+        display_window_ = display_window;
+        data_window_ = data_window;
+
+        int xtiles = std::ceil( ( double) ( data_window.size().x + 1) / tile_t::tile_size);
+        int ytiles = std::ceil( ( double) ( data_window.size().y + 1) / tile_t::tile_size);
+        int num_tiles = xtiles * ytiles;
+
+        if( tiles_.size() > num_tiles)
+            tiles_.resize( num_tiles);
+
+        for( int j = 0; j < ytiles; ++j)
+        {
+            for( int i = 0; i < xtiles; ++i)
+            {
+                if( i * j < tiles_.size())
+                {
+                    // reuse an existing tile
+                }
+                else
+                {
+                    // create a new tile
+                }
+            }
+        }
+    }
+
+    void draw() const
+    {
+        boost::range::for_each( tiles_, boost::bind( &tile_t::draw, _1));
+    }
+
+    math::box2i_t display_window_, data_window_;
+    image::buffer_t pixels_;
+    boost::ptr_vector<tile_t> tiles_;
+};
+
+image2_t::image2_t()
+{
+    pimpl_.reset( new impl());
+}
+
+image2_t::image2_t( image::buffer_t pixels,
+                    const math::box2i_t& display_window,
+                    const math::box2i_t& data_window)
+{
+    pimpl_.reset( new impl());
+    reset( pixels, display_window, data_window);
+}
+
+image2_t::~image2_t()
+{
+    // Do not remove.
+}
+
+void image2_t::reset()
+{
+    RAMEN_ASSERT( pimpl_.get());
+
+    pimpl_->reset();
+}
+
+void image2_t::reset( image::buffer_t pixels,
+                      const math::box2i_t& display_window,
+                      const math::box2i_t& data_window)
+{
+    RAMEN_ASSERT( pimpl_.get());
+
+    pimpl_->reset( pixels, display_window, data_window);
+}
+
+math::box2i_t image2_t::display_window() const
+{
+    RAMEN_ASSERT( pimpl_.get());
+
+    return pimpl_->display_window_;
+}
+
+math::box2i_t image2_t::data_window() const
+{
+    RAMEN_ASSERT( pimpl_.get());
+
+    return pimpl_->data_window_;
+}
+
+void image2_t::draw() const
+{
+    RAMEN_ASSERT( pimpl_.get());
+
+    pimpl_->draw();
+}
+
+} // ui
+} // ramen
